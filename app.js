859dcaf1d40b2268e4e862d0de080059235502d8
Alejandro Morgan <unciafidelis@gmail.com>
2022-04-27 11:37:40 -0500

    Add: Reader Class Unit Test Passed

--- app.js
+++ app.js
@@ -32,8 +32,9 @@ const assignFizzTrick = function(explorer){
 
 const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer));
 
-// Part 6: Get a new list of explorers in node if the score number is divisible by 5, we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
-//
+// Part 6: Get a new list of explorers in node if the score number is divisible by 5, 
+//we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
+
 const assignBuzzTrick = function(explorer){
     if(explorer.score%5 === 0){
         explorer.trick = "BUZZ";
@@ -46,7 +47,8 @@ const assignBuzzTrick = function(explorer){
 
 const explorersInNodeAndBuzzTrick = explorersInNode.map((explorer) => assignBuzzTrick(explorer));
 
-//Part7: Get a new list of explorers in Node, if the score number is divisible by 3 AND by 5 we need to set a new property called FIZZBUZZ, if not this value should be the same score value
+//Part7: Get a new list of explorers in Node, if the score number is divisible by 3 AND by 5 
+//we need to set a new property called FIZZBUZZ, if not this value should be the same score value
 
 const assignFizzBuzzTrick = function(explorer){
     if(explorer.score%5 === 0 && explorer.score%3 === 0){
